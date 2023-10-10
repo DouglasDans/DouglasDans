b22be8cef94aa38b1be51d3508cf4f4c897771c2
douglasdans <douglas.dans@gmail.com>
2023-10-09 22:46:57 -0300

    SobreMim Section | Componente adicionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Main from "@/components/Main"
+import SobreMim from "@/components/SobreMim/SobreMim"
 import React, { Fragment } from "react"
 
 const Home: React.FC = () => {
   return (
     <Fragment>
       <Main/>
-
+      <SobreMim/>
     </Fragment>
   )
 }
---
+++ src/components/SobreMim/SobreMim.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  './SobreMim.css'
+
+type Props = {}
+
+export default function SobreMim({}: Props) {
+   return (
+      <section className='sobre-mim-container'>
+         <h1>Sobre mim_</h1>
+         <p>aaaaaaaaaaaaaaaaaaaaaaa</p>
+      </section>
+   )
+}
