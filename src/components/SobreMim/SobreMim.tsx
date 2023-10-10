5ab4a3bcf225be1f1ddd55a25815e08cd0218b03
douglasdans <douglas.dans@gmail.com>
2023-10-09 22:53:14 -0300

    SobreMim Section | Alterado o margin do parágrafo

--- src/components/SobreMim/SobreMim.tsx
+++ src/components/SobreMim/SobreMim.tsx
@@ -7,7 +7,7 @@ export default function SobreMim({}: Props) {
    return (
       <section className='sobre-mim-container'>
          <h1>Sobre mim_</h1>
-         <p>aaaaaaaaaaaaaaaaaaaaaaa</p>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porta lorem quam. Ut lacinia dapibus orci, non gravida massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean dui quam, suscipit vitae interdum sed, mattis vitae arcu.</p>
       </section>
    )
 }
