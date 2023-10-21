ff14a996e262e7c5ae3ac3ab4d06141e7bf2849d
douglasdans <douglas.dans@gmail.com>
2023-10-20 23:16:22 -0300

    Portfolio | Criado layout dos cards e conexão com o JSON

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -4,21 +4,24 @@ import Card from './Card/Card'
 
 import './Portfolio.css'
 
+import portfolio from "@/json/portfolio.json"
+
 type Props = {}
 
 export default function Portfolio({}: Props) {
    return (
       <Container>
          <div className="portfolio-container" id='portfolio'>
-            <h2>Meu Portfólio</h2>
+            <h2>Meu Portfólio<span className='font-blue-color'>_</span></h2>
             <span>Meus projetos pessoais, acadêmicos e profissionais em que eu já trabalhei</span>
             <div className="portfolio-card-container">
-               <Card/>
-               <Card/>
-               <Card/>
-               <Card/>
-               <Card/>
-               <Card/>
+            {
+               portfolio.map(item => {
+                  return(
+                     <Card key={item.id} {...item} />
+                  )
+               })
+            }
             </div>
          </div>
       </Container>
