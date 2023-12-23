3d6e3d16f443dfea2690ac0cb1a21c9e76145abe
douglasdans <douglas.dans@gmail.com>
2023-12-22 22:26:30 -0300

    Habilidades | Adicionado novo layout e mais informações

--- src/app/components/Habilidades/Card/Card.tsx
+++ src/app/components/Habilidades/Card/Card.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import './Card.css'
 
 type Props = {
-   key: Number,
    icon: String,
-   title: String,
    desc: String
- }
+}
 
-export default function Card({ key, icon, title, desc } : Props) {
+export default function Card({icon, desc } : Props) {
    return (
       <div className='card-container card-habilidades'>
          <span className="material-symbols-rounded">{icon}</span>
-         <h4>{title}</h4>
          <span>{desc}</span>
       </div>
    )
--- src/app/components/Habilidades/Habilidades.tsx
+++ src/app/components/Habilidades/Habilidades.tsx
@@ -8,14 +8,13 @@ type Props = {}
 
 export default function Habilidades({ } : Props) { 
    
-   const mapCards = habilidades.map(item => {
-      return(<Card key={item.id} icon={item.icon} title={item.title} desc={item.desc}/>)
+   const mapCards = habilidades.map((item, index) => {
+      return(<Card key={index} icon={item.icon} desc={item.desc}/>)
    })
 
    return (
       <div className="habilidades-container" id='habilidades'>
-         <h2>Habilidades<span className='font-blue-color'>_</span></h2>
-
+         <h2>Minhas Habilidades<span className='font-blue-color'>_</span></h2>
          <div className="cards-container">
             {mapCards}
          </div>
