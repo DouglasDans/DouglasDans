08b11c1917970424e4a2047cc9840e5e0833c859
douglasdans <douglas.dans@gmail.com>
2024-08-05 20:30:18 -0300

    sobre mim | nova descrição

--- src/components/home/SobreMim/SobreMim.tsx
+++ src/components/home/SobreMim/SobreMim.tsx
@@ -9,7 +9,7 @@ export default function SobreMim({}: Props) {
       <section className={styles.container} id='sobre-mim'>
          <h2>Sobre mim<span className='font-blue-color'>_</span></h2>
          <p>
-            Desde 2019, tenho me dedicado ao estudo e aprimoramento para me tornar um desenvolvedor, expandindo minhas habilidades em frontend e backend. <br/> Ao longo desse período, desenvolvi diversos projetos para enriquecer minha experiência teórica e prática. Atualmente, estou disponível para contratação e também para projetos freelancer, ansioso para aplicar todo o conhecimento adquirido ao longo desses anos e alcançar ainda mais na minha jornada profissional.
+            Trabalho ativamente no meu crescimento profissional, por meio de projetos e estudos onde posso praticar todo o meu conhecimento. Mas também trabalho no meu desenvolvimento pessoal, lendo livros e assistindo a vídeos inspiradores que possam me tornar um ser humano melhor. Sempre estarei aberto a adquirir novos conhecimentos e experiências e a conhecer novos pontos de vista.
          </p>
       </section>
    )
