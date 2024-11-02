071d212a7380b4acb688094cacbca2fe8f5f8bd1
douglasdans <douglas.dans@gmail.com>
2024-11-02 16:32:27 -0300

    feat: adiciona reboo ao portfólio

---
+++ src/app/projetos/[id]/page.tsx
@@ -0,0 +1,10 @@
+import {Fragment} from "react";
+import Header from "@/components/home/Main/Header/Header";
+
+export default function ProjetoPage({params}){
+   return(
+      <Fragment>
+         <Header/>
+      </Fragment>
+   )
+}
