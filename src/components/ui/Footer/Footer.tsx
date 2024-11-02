cbb075f50f4efddb7ebf3d897d49d8925129fa4b
douglasdans <douglas.dans@gmail.com>
2024-11-02 16:33:49 -0300

    fix: corrige links do footer

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -6,29 +6,29 @@ import SocialLinks from '@/components/ui/SocialLinks/SocialLinks'
 
 type Props = {}
 
-export default function Footer({}: Props) {
+export default function Footer({ }: Props) {
    return (
       <footer className='footer-container'>
          <h3 className='title-name'><span className='font-blue-color'>{">>"}</span> Douglas Nascimento<span className='font-blue-color'>_</span></h3>
 
          <nav className="footer-nav-links">
-            <Link href="/public#">
+            <Link href="/#">
                <h5>Voltar ao topo</h5>
             </Link>
-            <Link href="/public#habilidades">
+            <Link href="/#habilidades">
                <h5>Minhas Habilidades</h5>
             </Link>
-            <Link href="/public#tecnologias">
+            <Link href="/#tecnologias">
                <h5>Minhas Tecnologias</h5>
             </Link>
-            <Link href="/public#portfolio">
+            <Link href="/#portfolio">
                <h5>Meu Portfolio</h5>
             </Link>
          </nav>
 
          <div className="footer-right">
-            <ButtonLinks flexColumn={true}/>
-            <SocialLinks/>
+            <ButtonLinks flexColumn={true} />
+            <SocialLinks />
          </div>
       </footer>
    )
