11747ede7b0b5dcd6073f5d925c4e274995a5b68
douglasdans <douglas.dans@gmail.com>
2025-04-11 21:30:07 -0300

    fix: corrige errros de deploy

--- src/components/home/Tecnologias/Tecnologias.tsx
+++ src/components/home/Tecnologias/Tecnologias.tsx
@@ -6,20 +6,20 @@ import tecnologias from '@/json/tecnologias.json'
 
 type Props = {}
 
-export default function Tecnologias({}: Props) {
+export default function Tecnologias({ }: Props) {
 
    const mapTech = tecnologias.map((item, index) => {
-      return(
-         <Card key={index} nome={item.nome} estudandoHoje={item.estudandoHoje} nivelHabilidade={item.nivelHabilidade} imgName={item.imgName}/>
+      return (
+         <Card key={index} nome={item.nome} nivelHabilidade={item.nivelHabilidade} imgName={item.imgName} />
       )
    })
 
    return (
-    <div className='tecnologias-container' id='tecnologias'>
-      <h2>Minhas Principais Tecnologias<span className='font-blue-color'>_</span></h2>
-      <div className="cards-container">
-         {mapTech}
+      <div className='tecnologias-container' id='tecnologias'>
+         <h2>Minhas Principais Tecnologias<span className='font-blue-color'>_</span></h2>
+         <div className="cards-container">
+            {mapTech}
+         </div>
       </div>
-    </div>
-  )
+   )
 }
