8f760498d198b7a2d98fb4555ade52810f02a38e
douglasdans <douglas.dans@gmail.com>
2025-04-14 10:18:09 -0300

    feat: adiciona componente NavbarWebspace com links de navegação

---
+++ src/components/navbar-webspace/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import TitleDecorator from '../title-decorator'
+import styles from './index.module.scss'
+import Link from 'next/link'
+
+export default function NavbarWebspace() {
+  return (
+    <nav className={styles.container}>
+      <h1><TitleDecorator text='Douglas Nacimento' /></h1>
+      <div className={styles.linksWrapper}>
+        <Link href={'#'}>Sobre Mim</Link>
+        <Link href={'#'}>Habilidades</Link>
+        <Link href={'#'}>Portfólio</Link>
+        {/* <Link href={'#'}>Blog</Link> */}
+      </div>
+    </nav>
+  )
+}
