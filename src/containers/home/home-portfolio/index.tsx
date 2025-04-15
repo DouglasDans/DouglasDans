0aae3d80c265f8ee5e7cd0dc51514c6f39643234
douglasdans <douglas.dans@gmail.com>
2025-04-15 15:38:35 -0300

    feat: transforma botão em link para navegação ao portfólio completo

--- src/containers/home/home-portfolio/index.tsx
+++ src/containers/home/home-portfolio/index.tsx
@@ -2,13 +2,14 @@ import TitlePipe from "@/components/title-pipe";
 import styles from './index.module.scss';
 import ProjectCard from "@/components/project-card";
 import Button from "@/components/button";
+import Link from "next/link";
 
 export default function PortfolioHome() {
   return (
     <section id='habilidades' className={styles.container}>
       <div className={styles.header}>
         <h2><TitlePipe text="Meu Portfólio" /></h2>
-        <Button>Ver Portfólio Completo</Button>
+        <Link href={"/portfolio"}><Button>Ver Portfólio Completo</Button></Link>
       </div>
 
       <div className={styles.itemsWrapper}>
