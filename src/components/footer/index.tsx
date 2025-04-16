621424dc788dafff7e0cba4fe138f4e3d4a59040
douglasdans <douglas.dans@gmail.com>
2025-04-16 09:12:57 -0300

    refactor: corrige alinhamento do conteúdo do footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,29 +7,33 @@ import SocialLinksWrapper from '../social-links-wrapper'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <h3 className={styles.title}><TitlePipe text="Douglas Nascimento" /></h3>
+      <div className={`${styles.content} ${styles.left}`}>
+        <h3 className={styles.title}><TitlePipe text="Douglas Nascimento" /></h3>
 
-      <div className={styles.links}>
-        <Link href={"#top"}>Voltar ao Topo</Link>
-        <Link href={"#sobre-mim"}>Sobre Mim</Link>
-        <Link href={"#habilidades"}>Minhas Habilidades</Link>
-        <Link href={"#postfolio"}>Meu Portfólio</Link>
+        <div className={styles.links}>
+          <Link href={"#top"}>Voltar ao Topo</Link>
+          <Link href={"#sobre-mim"}>Sobre Mim</Link>
+          <Link href={"#habilidades"}>Minhas Habilidades</Link>
+          <Link href={"#postfolio"}>Meu Portfólio</Link>
+        </div>
       </div>
 
-      <div className={styles.buttonsWrapper}>
-        <Link href={"/blog"}>
-          <Button size='sm'>Meu Blog</Button>
-        </Link>
-        <Link href={"/curriculo"}>
-          <Button size='sm'>Meu Currículo</Button>
-        </Link>
-        <Link href={"/portfolio"}>
-          <Button size='sm'>Meu Portfólio</Button>
-        </Link>
-      </div>
+      <div className={`${styles.content} ${styles.right}`}>
+        <div className={styles.buttonsWrapper}>
+          <Link href={"/blog"}>
+            <Button size='sm'>Meu Blog</Button>
+          </Link>
+          <Link href={"/curriculo"}>
+            <Button size='sm'>Meu Currículo</Button>
+          </Link>
+          <Link href={"/portfolio"}>
+            <Button size='sm'>Meu Portfólio</Button>
+          </Link>
+        </div>
 
-      <div className={styles.socialLinks}>
-        <SocialLinksWrapper />
+        <div>
+          <SocialLinksWrapper />
+        </div>
       </div>
     </footer>
   )
