12717d7f52b886a2cafc599077166da5ea13e18e
douglasdans <douglas.dans@gmail.com>
2025-04-21 18:51:46 -0300

    refactor: reorganiza ModalTrigger para dentro de um Suspense para que ele possa ser executado corretamente após o deploy

--- src/components/modal/trigger.tsx
+++ src/components/modal/trigger.tsx
@@ -2,10 +2,9 @@
 
 import { useSearchParams } from "next/navigation"
 import Modal from "."
+import { Suspense } from "react"
 
-type Props = {}
-
-export default function ModalTrigger({ }: Props) {
+function ModalContent() {
   const params = useSearchParams()
   const modalOpen: boolean = (params.get('modal') ?? false) as boolean
   const projectId = params.get('project-id')
@@ -13,4 +12,14 @@ export default function ModalTrigger({ }: Props) {
   if (modalOpen && projectId) {
     return <Modal projectId={projectId} />
   }
+
+  return null
+}
+
+export default function ModalTrigger() {
+  return (
+    <Suspense fallback={null}>
+      <ModalContent />
+    </Suspense>
+  )
 }
