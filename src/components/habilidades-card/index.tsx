70ae32e5f273c790ead91fb187d5b7c6e1e71d8f
douglasdans <douglas.dans@gmail.com>
2025-04-22 11:44:29 -0300

    feat: atualiza componente HabilidadesCard para incluir título e estrutura de conteúdo

--- src/components/habilidades-card/index.tsx
+++ src/components/habilidades-card/index.tsx
@@ -2,14 +2,18 @@ import styles from './index.module.scss'
 
 type Props = {
   icon: string
+  title: string
   text: string
 }
 
-export default function HabilidadesCard({ icon, text }: Readonly<Props>) {
+export default function HabilidadesCard({ icon, text, title }: Readonly<Props>) {
   return (
     <div className={styles.card}>
       <span className={`material-symbols-rounded ${styles.icon}`}>{icon}</span>
-      <span>{text}</span>
+      <div className={styles.content}>
+        <span>{title}</span>
+        <small>{text}</small>
+      </div>
     </div>
   )
 }
--- src/containers/home/habilidades/index.tsx
+++ src/containers/home/habilidades/index.tsx
@@ -1,6 +1,7 @@
 import TitlePipe from '@/components/title-pipe'
 import styles from './index.module.scss'
 import HabilidadesCard from '@/components/habilidades-card'
+import skills from "@/data/skills.json"
 
 export default function Habilidades() {
   return (
@@ -8,7 +9,9 @@ export default function Habilidades() {
       <h2><TitlePipe text="Minhas Habilidades" /></h2>
 
       <div className={styles.itemsWrapper}>
-        <HabilidadesCard icon='delete' text='nadjfbsafhd.rjbafjl ' />
+        {skills.map((skill, index) => {
+          return <HabilidadesCard icon={skill.icon} title={skill.title} text={skill.desc} key={index} />
+        })}
       </div>
     </section>
   )
