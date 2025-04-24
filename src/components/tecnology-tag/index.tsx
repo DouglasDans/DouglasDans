803159add28b30a5f21e6f1c7548c1c6586a65ac
douglasdans <douglas.dans@gmail.com>
2025-04-24 12:29:03 -0300

    fix: adiciona tratamento de erro ao carregar icones não disponíveis nas tags de tecnologia

--- src/components/tecnology-tag/index.tsx
+++ src/components/tecnology-tag/index.tsx
@@ -1,22 +1,28 @@
 import Image from "next/image"
+import { useState } from "react"
 import styles from './index.module.scss'
 
 type Props = {
   values: string[]
 }
 
-export default function TechnologyTag({ values }: Readonly<Props>) {
+export default function TechnologyTag({ values }: Props) {
   return (
     <div className={styles.container}>
       {values.map((value, index) => {
+        const [imageError, setImageError] = useState(false);
+
         return (
           <small className={styles.tag} key={index}>
-            <Image
-              src={`/tech-logos/${value.toLowerCase()}.svg`}
-              height={15}
-              width={15}
-              alt={`${value} tech logo`}
-            />
+            {!imageError && (
+              <Image
+                src={`/tech-logos/${value.toLowerCase()}.svg`}
+                height={15}
+                width={15}
+                alt={`${value} tech logo`}
+                onError={() => setImageError(true)}
+              />
+            )}
             {value}
           </small>
         )
