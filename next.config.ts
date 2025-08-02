089dea8d438696164aab60b3d8f800ee5f8e5e1c
douglasdans <douglas.dans@gmail.com>
2025-08-02 18:53:48 -0300

    fix: corrige a exibição de imagens do notion nos cards da landing page

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,16 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com", "www.notion.so"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.notion.so",
+      },
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+      },
+    ],
   },
 };
 
